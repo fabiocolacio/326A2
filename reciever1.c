13b835158f36a376917022e553331e6fc46c34a5
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-10-25 12:17:46 -0700

    Close message queue if last program to terminate

--- reciever1.c
+++ reciever1.c
@@ -16,6 +16,7 @@ main (int   argc,
     key_t key = ftok ("/home", 1);
     int   id = msgget (key, msgflag);
 
+    int   dead_programs = 0;
     int   dead_senders = 0;
 
     if (id == -1) {
@@ -40,7 +41,16 @@ main (int   argc,
         }
         else if (msg.data == -997 || msg.data == -251) {
             dead_senders += 1;
+            dead_programs += 1;
         }
+        else if (msg.data == -2 || msg.data == -257) {
+            dead_programs += 1; 
+        }
+    }
+
+    if (dead_programs >= 4) {
+        // We are the last program alive. Deallocate the queue here.
+        msgctl (id, IPC_RMID, NULL);
     }
 
     return 0;
