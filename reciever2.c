8a0cc8be3d1b126e83b35c0b78487c0a08b5195f
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-10-30 11:24:49 -0700

    Update reciever2 to use sender field.

--- reciever2.c
+++ reciever2.c
@@ -11,12 +11,14 @@ int
 main (int   argc,
       char *argv)
 {
-    int   msgflag = IPC_CREAT | IPC_EXCL | 0666;
+    int   msgflag = IPC_CREAT | 0666;
 
     key_t key = ftok ("/home", 1);
     int   id = msgget (key, msgflag);
 
     int   total_messages = 0;
+
+    int   dead_programs = 0;
 
     if (id == -1) {
         return -1;
@@ -24,22 +26,32 @@ main (int   argc,
 
     srand (time (0));
 
-    while (total_messages < 5000) {
+    while (dead_programs < 4 && total_messages < 5000) {
         event msg;
 
         msgrcv (id, &msg, EVENT_SIZE, 2, 0);
 
-        if (msg.data % 997 == 0) {
+        if (msg.sender == 997) {
             printf ("Sender 997: %d\n", msg.data);
             total_messages += 1;
             msg.type = 997;
-            msg.data = rand ();
+            msg.data = 2 * rand ();
             msgsnd (id, &msg, EVENT_SIZE, 0);
         }
-        else if (msg.data % 257 == 0) {
+        else if (msg.sender == 257) {
             printf ("Sender 257: %d\n", msg.data);
             total_messages += 1;
         }
+        else if (msg.sender == -997 ||
+                 msg.sender == -225 ||
+                 msg.sender == -251 ||
+                 msg.sender == -1) {
+            dead_programs += 1;
+        }
+    }
+
+    if (dead_programs >= 4) {
+        msgctl (id, IPC_RMID, NULL);
     }
 
     return 0;
