dbbe36e595bf4b3485afdb167035a4357f63c948
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-10-30 11:19:55 -0700

    Add sender field to event.h

--- event.h
+++ event.h
@@ -1,11 +1,12 @@
 #ifndef __EVENT_H__
 #define __EVENT_H__
 
-#define EVENT_SIZE sizeof (long)
+#define EVENT_SIZE sizeof (long) + sizeof (int)
 
 typedef struct {
     long type;
     long data;
+    int  sender;
 } event;
 
 #endif
