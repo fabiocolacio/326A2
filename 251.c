8035f7c50acf859b258a4edbfaa542a47666cbd3
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-11-04 22:33:49 -0800

    Cleanup 251.c

--- 251.c
+++ 251.c
@@ -1,7 +1,6 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
@@ -19,51 +18,19 @@ main (int   argc,
     int msgflag = IPC_CREAT | 0666;
     key_t key = ftok ("/home", 1);
     int id = msgget (key, msgflag);
-    int dead_programs = 0;
+    if (id == -1) return 1;
 
     event msg;
     msg.type = 1;
     msg.sender = -MARKER;
     get_info (id, (msgbuf*) &msg, EVENT_SIZE, 1);
 
-    if (id == -1) {
-        return 1;
-    }
-
     srand (time (0));
 
-    while (dead_programs < 4) {
-        ssize_t err = 0;
-
-        // Send a random message
+    while (1) {
         msg.type = 1;
-        msg.data = rand () * MARKER;
+        msg.data = rand ();
         msg.sender = MARKER;
-        msgsnd (id, &msg, EVENT_SIZE, 0);
-
-        msg.type = MARKER;
-        msgsnd (id, &msg, EVENT_SIZE, 0);
-        msgrcv (id, &msg, EVENT_SIZE, MARKER, 0);
-        if (msg.sender < 0)
-            dead_programs += 1;
-    }
-
-    if (dead_programs >= 4) {
-        msgctl (id, IPC_RMID, NULL);
-    }
-    else {
-        msg.sender = -251;
-
-        msg.type = 1;
-        msgsnd (id, &msg, EVENT_SIZE, 0);
-
-        msg.type = 2;
-        msgsnd (id, &msg, EVENT_SIZE, 0);
-
-        msg.type = 997;
-        msgsnd (id, &msg, EVENT_SIZE, 0);
-
-        msg.type = 257;
         msgsnd (id, &msg, EVENT_SIZE, 0);
     }
 
